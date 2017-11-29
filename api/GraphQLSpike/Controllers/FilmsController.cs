d255831423b7be768d69bc9abf2fd30e240a799e
JvR <johan.vanrensburg@krs.co.za>
2017-11-29 15:03:04 +0200

    Create new endpoint for graphql

--- api/GraphQLSpike/Controllers/FilmsController.cs
+++ api/GraphQLSpike/Controllers/FilmsController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using GraphQL.Types;
-using GraphQL;
+using System;
 
 namespace GraphQLSpike.Controllers
 {
@@ -13,24 +8,9 @@ namespace GraphQLSpike.Controllers
   [Route("api/Films")]
   public class FilmsController : Controller
   {
-    private readonly ISchema _schema;
-
-    public FilmsController(ISchema schema)
-    {
-      _schema = schema;
-    }
-
     public async Task<ActionResult> Get()
     {
-      var result = _schema.Execute(conf =>
-      {
-        //conf.Query = "{ film(film_id: 133) { film_id title } }";
-        conf.Query = "{ films { film_id title } }";
-      });
-
-      Console.WriteLine(result);
-
-      return Ok(result);
+      throw new NotImplementedException();
     }
   }
 }
---
+++ api/GraphQLSpike/Controllers/GraphQLController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GraphQL.Types;
+using GraphQL;
+
+namespace GraphQLSpike.Controllers
+{
+  [Produces("application/json")]
+  [Route("api/GraphQL")]
+  public class GraphQLController : Controller
+  {
+    private readonly ISchema _schema;
+
+    public GraphQLController(ISchema schema)
+    {
+      _schema = schema;
+    }
+
+    public async Task<ActionResult> Get()
+    {
+      var result = _schema.Execute(conf =>
+      {
+        //conf.Query = "{ film(film_id: 133) { film_id title } }";
+        conf.Query = "{ films { film_id title } }";
+      });
+
+      Console.WriteLine(result);
+
+      return Ok(result);
+    }
+  }
+}
